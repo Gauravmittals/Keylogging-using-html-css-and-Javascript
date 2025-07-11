c8a30e0821468b2e90195bd5f4ca0fb7a8843b8d
gaurav mittal <gauravmittal@Gaurav-Mittal.local>
2025-07-11 23:37:15 +0530

    All Codes

---
+++ lecture1.js
@@ -0,0 +1,27 @@
+// getElementById()
+
+// getElementsByClassName()
+
+// getElementsByTagName()
+
+// querySelector() and querySelectorAll()
+
+
+const element = document.getElementsByTagName("li");
+console.log(element);
+
+const ele= document.getElementsByClassName('hero')
+ console.log(ele);
+
+ const ell=document.getElementById('hi');
+ console.log(ell);
+
+
+ const el= document.querySelector('.qs');
+ console.log(el);
+
+
+ const e=document.querySelectorAll('.qs');
+ console.log(e[0].textContent);
+
+ 
